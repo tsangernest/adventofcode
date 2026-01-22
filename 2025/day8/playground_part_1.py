163c9d29319f26e39c763536913ee00632b91a1e
p14s <tsangernest@hotmail.com>
2026-01-22 12:51:29 -0600

    i think we're okay with ignoring zero distances

--- 2025/day8/playground_part_1.py
+++ 2025/day8/playground_part_1.py
@@ -18,8 +18,12 @@ def main():
         for a, (x1, y1, z1) in enumerate(data):
             for b, (x2, y2, z2) in enumerate(data):
                 squares = pow(x1-x2, 2), pow(y1-y2, 2), pow(z1-z2, 2)
-                distances.append(sqrt(sum(squares)))
-        # Do I then sort, and then throw it into a set?
+                distance = sqrt(sum(squares))
+                if not distance:
+                    continue
+                distances.append([sqrt(sum(squares)), a, b])
+        # anything with zero distances we can just safely ignore
+
 
 if __name__ == "__main__":
     main()
